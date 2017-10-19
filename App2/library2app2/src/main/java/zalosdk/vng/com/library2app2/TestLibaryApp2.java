5fdf53ffd1355f3dc137eab699062f0402651452
thuannmvng <thuannm@vng.com.vn>
2017-10-19 15:22:21 +0700

    submit app2
    submit app2

---
+++ App2/library2app2/src/main/java/zalosdk/vng/com/library2app2/TestLibaryApp2.java
@@ -0,0 +1,12 @@
+package zalosdk.vng.com.library2app2;
+
+/**
+ * Created by CPU10329-local on 10/19/2017.
+ */
+
+public class TestLibaryApp2 {
+
+    public static void checkLibApp2(){
+
+    }
+}
